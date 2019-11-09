444ace748a044bcf555c41388f58f23046704938
Natalie Klinke <nk@10vorne.it>
2019-11-09 15:50:30 +0700

    test zoom level 10

--- src/Components/MapWrapper.js
+++ src/Components/MapWrapper.js
@@ -64,7 +64,7 @@ class MapWrapper extends Component {
       const mapNode = ReactDOM.findDOMNode(mapReference);
       const mapSettings = Object.assign({},{
         center: this.state.initialCenter,
-        zoom: 0
+        zoom: 10
       })
 
       this.map = new maps.Map(mapNode, mapSettings);
